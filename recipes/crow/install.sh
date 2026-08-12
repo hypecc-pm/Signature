4617e5e396050e2e1c54f57fe98b227504fbfdc9
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 16:15:32 +0300

    Update install.sh

--- recipes/crow/install.sh
+++ recipes/crow/install.sh
@@ -1,1 +1,36 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    BUILD_DIR="/tmp/crow_build"
+    rm -rf "${BUILD_DIR}"
+
+    git clone --depth 1 https://github.com/CrowCpp/Crow.git "${BUILD_DIR}"
+    cmake -B "${BUILD_DIR}/build" -S "${BUILD_DIR}" -DCMAKE_BUILD_TYPE=Release -DCROW_BUILD_EXAMPLES=OFF -DCROW_BUILD_TESTS=OFF
+    cmake --install "${BUILD_DIR}/build"
+
+    mkdir -p /var/lib/signature/manifests
+    MANIFEST="/var/lib/signature/manifests/crow.list"
+    rm -f "${MANIFEST}"
+
+    if [ -d "/usr/local/include/crow" ]
+    then
+        find /usr/local/include/crow -type f > "${MANIFEST}"
+    fi
+
+    if [ -f "/usr/local/include/crow.h" ]
+    then
+        echo "/usr/local/include/crow.h" >> "${MANIFEST}"
+    fi
+
+    if [ -d "/usr/local/lib/cmake/Crow" ]
+    then
+        find /usr/local/lib/cmake/Crow -type f >> "${MANIFEST}"
+    fi
+
+    rm -rf "${BUILD_DIR}"
+    echo "[crow] Header library deployed successfully."
+}
+
+main
