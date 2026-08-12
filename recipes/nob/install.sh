fefe09e4c89358394b698a416eb71dec8b65bbf1
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 16:18:40 +0300

    Update install.sh

--- recipes/nob/install.sh
+++ recipes/nob/install.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    BUILD_DIR="/tmp/nob_build"
+    rm -rf "${BUILD_DIR}"
+
+    git clone --depth 1 https://github.com/tsoding/nob.h.git "${BUILD_DIR}"
+    
+    mkdir -p /usr/local/include
+    cp "${BUILD_DIR}/nob.h" /usr/local/include/nob.h
+
+    mkdir -p /var/lib/signature/manifests
+    MANIFEST="/var/lib/signature/manifests/nob.list"
+    rm -f "${MANIFEST}"
+
+    echo "/usr/local/include/nob.h" > "${MANIFEST}"
+
+    rm -rf "${BUILD_DIR}"
+    echo "[nob] Header library deployed successfully."
+}
+
+main
