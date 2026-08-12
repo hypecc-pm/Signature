9fdf157bfeee332163d11bcdce19aecfde5a3a83
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 15:40:54 +0300

    Update remove.sh

--- recipes/cpr/remove.sh
+++ recipes/cpr/remove.sh
@@ -1,1 +1,25 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    if [ -f "/var/lib/signature/manifests/cpr.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/cpr.list"
+        rm -f "/var/lib/signature/manifests/cpr.list"
+    fi
+
+    rm -rf /usr/local/include/cpr
+    rm -rf /usr/local/lib/cmake/cpr
+    rm -f /usr/local/lib/libcpr*
+
+    echo "[cpr] Target libraries and manifest entries purged successfully."
+}
+
+main
