7a100483f1d7dbccff76bc1d44ca1859054b45b5
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 16:14:48 +0300

    Update remove.sh

--- recipes/imgui/remove.sh
+++ recipes/imgui/remove.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    if [ -f "/var/lib/signature/manifests/imgui.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/imgui.list"
+        rm -f "/var/lib/signature/manifests/imgui.list"
+    fi
+
+    rm -rf /usr/local/include/imgui
+    rm -f /usr/local/lib/libimgui.so
+
+    echo "[imgui] Target libraries and manifest entries purged successfully."
+}
+
+main
