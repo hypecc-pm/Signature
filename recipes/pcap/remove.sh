c18d5375678f4b9f39556646693f0fe97658fe38
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 15:50:54 +0300

    Update remove.sh

--- recipes/pcap/remove.sh
+++ recipes/pcap/remove.sh
@@ -1,1 +1,25 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    if [ -f "/var/lib/signature/manifests/pcap.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/pcap.list"
+        rm -f "/var/lib/signature/manifests/pcap.list"
+    fi
+
+    rm -f /usr/local/include/pcap.h
+    rm -rf /usr/local/include/pcap
+    rm -f /usr/local/lib/libpcap*
+
+    echo "[pcap] Target libraries and manifest entries purged successfully."
+}
+
+main
