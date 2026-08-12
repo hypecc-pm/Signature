c87ccfb24ac8853a4f56df1ccf34dcfd6b21c904
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 16:08:54 +0300

    Update remove.sh

--- recipes/sqlite_orm/remove.sh
+++ recipes/sqlite_orm/remove.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    if [ -f "/var/lib/signature/manifests/sqlite_orm.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/sqlite_orm.list"
+        rm -f "/var/lib/signature/manifests/sqlite_orm.list"
+    fi
+
+    rm -rf /usr/local/include/sqlite_orm
+    rm -rf /usr/local/lib/cmake/sqlite_orm
+
+    echo "[sqlite_orm] Target headers and manifest entries purged successfully."
+}
+
+main
