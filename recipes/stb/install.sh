601ad9c088503653638f45a6d5cf7e49678b5d50
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 17:13:56 +0300

    Update install.sh

--- recipes/stb/install.sh
+++ recipes/stb/install.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    BUILD_DIR="/tmp/stb_build"
+    rm -rf "${BUILD_DIR}"
+
+    git clone --depth 1 https://github.com/nothings/stb.git "${BUILD_DIR}"
+
+    mkdir -p /usr/local/include/stb
+    cp "${BUILD_DIR}"/*.h /usr/local/include/stb/
+
+    mkdir -p /var/lib/signature/manifests
+    MANIFEST="/var/lib/signature/manifests/stb.list"
+    rm -f "${MANIFEST}"
+
+    find /usr/local/include/stb -type f > "${MANIFEST}"
+
+    rm -rf "${BUILD_DIR}"
+    echo "[stb] Single-file header collection deployed successfully."
+}
+
+main
