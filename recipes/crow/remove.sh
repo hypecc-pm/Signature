18f3baecd73fbc30a45a0b6d9fdc446cd65cb01d
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 16:15:42 +0300

    Update remove.sh

--- recipes/crow/remove.sh
+++ recipes/crow/remove.sh
@@ -1,1 +1,25 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    if [ -f "/var/lib/signature/manifests/crow.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/crow.list"
+        rm -f "/var/lib/signature/manifests/crow.list"
+    fi
+
+    rm -rf /usr/local/include/crow
+    rm -f /usr/local/include/crow.h
+    rm -rf /usr/local/lib/cmake/Crow
+
+    echo "[crow] Target headers and manifest entries purged successfully."
+}
+
+main
