741af5226f38cf241cf4058942342dfff1995b5f
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 15:20:35 +0300

    Update remove.sh

--- recipes/ftxui/remove.sh
+++ recipes/ftxui/remove.sh
@@ -1,1 +1,25 @@
 #!/usr/bin/env bash
+set -e
+
+main()
+{
+    if [ -f "/var/lib/signature/manifests/ftxui.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/ftxui.list"
+        rm -f "/var/lib/signature/manifests/ftxui.list"
+    fi
+
+    rm -rf /usr/local/include/ftxui
+    rm -rf /usr/local/lib/cmake/ftxui
+    rm -f /usr/local/lib/libftxui*
+
+    echo "[ftxui] Target libraries and manifest entries purged successfully."
+}
+
+main
