f9a6ab6aea6090089631ec5becc08e0e96d5c527
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 17:14:26 +0300

    Update remove.sh

--- recipes/eigen/remove.sh
+++ recipes/eigen/remove.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    if [ -f "/var/lib/signature/manifests/eigen.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/eigen.list"
+        rm -f "/var/lib/signature/manifests/eigen.list"
+    fi
+
+    rm -rf /usr/local/include/eigen3
+    rm -rf /usr/local/share/eigen3
+
+    echo "[eigen] Target headers and manifest entries purged successfully."
+}
+
+main
