980ff684535a532c0fbeda5e7f2511acccd8da63
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 16:07:27 +0300

    Update remove.sh

--- recipes/sqlite3/remove.sh
+++ recipes/sqlite3/remove.sh
@@ -1,1 +1,26 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    if [ -f "/var/lib/signature/manifests/sqlite3.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/sqlite3.list"
+        rm -f "/var/lib/signature/manifests/sqlite3.list"
+    fi
+
+    rm -f /usr/local/include/sqlite3.h
+    rm -f /usr/local/include/sqlite3ext.h
+    rm -f /usr/local/bin/sqlite3
+    rm -f /usr/local/lib/libsqlite3*
+
+    echo "[sqlite3] Target libraries and manifest entries purged successfully."
+}
+
+main
