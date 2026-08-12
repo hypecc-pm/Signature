f20165f790ff5b980eea80b5416a3c3b3d8dc712
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-08-12 17:13:47 +0300

    Update remove.sh

--- recipes/stb/remove.sh
+++ recipes/stb/remove.sh
@@ -1,1 +1,23 @@
+#!/usr/bin/env bash
+set -e
 
+main()
+{
+    if [ -f "/var/lib/signature/manifests/stb.list" ]
+    then
+        while IFS= read -r file
+        do
+            if [ -f "${file}" ]
+            then
+                rm -f "${file}"
+            fi
+        done < "/var/lib/signature/manifests/stb.list"
+        rm -f "/var/lib/signature/manifests/stb.list"
+    fi
+
+    rm -rf /usr/local/include/stb
+
+    echo "[stb] Target headers and manifest entries purged successfully."
+}
+
+main
